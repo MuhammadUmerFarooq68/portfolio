64b06ffdadd0865803d96bcca4135b10a9928ed2
umerfarooq68 <engr.umerfarooq68@gmail.com>
2024-07-30 14:30:18 +0500

    aaded new phenologics experience

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -7,31 +7,31 @@ export const experiences = [
     
   },
   {
-    id: 1,
+    id: 2,
     title: 'MERN Stack Developer ',
     company: "Webbuggs",
     duration: "(March-2024- July-2024)"
   },
   {
-    id: 2,
+    id: 3,
     title: "Software Enginner",
     company: "GCS PVT LTD",
     duration: "(Feb-2023- Jan-2024)"
   },
   {
-    id: 3,
+    id: 4,
     title: "MERN stack developer",
     company: "PITB",
     duration: "(Jan-2024- March-2024)"
   },
   {
-    id: 4,
+    id: 5,
     title: "MERN stack developer",
     company: "GrowIntern",
     duration: "(Feb-2023- July-2023)"
   },
   {
-    id: 3,
+    id: 6,
     title: "EVS",
     company: "MERN stack developer",
     duration: "(April-2022-Feb-2023)"
